9c5c78ce4bd0350ee2f1f850cef9cf454617366f
etterwin <etterwin.yll@gmail.com>
2018-02-20 18:09:38 +0300

    update

--- land_app/forms.py
+++ land_app/forms.py
@@ -3,6 +3,14 @@ from django import forms
 
 class NamePhoneForm(forms.Form):
     name = forms.CharField(label='name_field', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Введите Ваше имя', 'class': 'field'}))
+    phone = forms.CharField(label='number_field', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Введите Ваш номер', 'class': 'field'}))
+
+
+class DetailPhoneForm(forms.Form):
     detail = forms.CharField(label='name_field', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Введите название детали', 'class': 'field'}))
-    quiz = forms.CharField(label='name_field', max_length=300, widget=forms.TextInput(attrs={'placeholder': 'Введите текст вопроса', 'class': 'footer-field'}))
+    phone = forms.CharField(label='number_field', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Введите Ваш номер', 'class': 'field'}))
+
+
+class QuizPhoneForm(forms.Form):
+    quiz = forms.CharField(label='name_field', max_length=300, widget=forms.TextInput(attrs={'placeholder': 'Введите текст вопроса', 'class': 'field'}))
     phone = forms.CharField(label='number_field', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Введите Ваш номер', 'class': 'field'}))
--- land_app/views.py
+++ land_app/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render
-from .forms import NamePhoneForm
+from .forms import NamePhoneForm, DetailPhoneForm, QuizPhoneForm
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
 
 
 class IndexView(View):
     template_name = 'index.html'
 
+    @csrf_exempt
     def get(self, request):
-        form = NamePhoneForm()
-        return render(request, self.template_name, {'form': form})
+        forms = NamePhoneForm(), DetailPhoneForm(), QuizPhoneForm()
 
+        return render(request, self.template_name, {'form_1': forms[0], 'form_2': forms[1], 'form_3': forms[2]})
+
+    @csrf_exempt
     def post(self, request):
-        form = NamePhoneForm(request.POST)
 
-        if form.is_valid():
-            print(form.cleaned_data)
+        # if 'namephoneform' in request.POST:
+        #     form = NamePhoneForm(request.POST)
+        # elif 'detailphoneform' in request.POST:
+        #     form = DetailPhoneForm(request.POST)
+        # else:
+        #     form = QuizPhoneForm(request.POST)
+        #
+        # if form.is_valid():
+        #     print(form.cleaned_data)
+        #
+        #     return render(request, self.template_name, {
+        #         'form_1': NamePhoneForm(),
+        #         'form_2': DetailPhoneForm(),
+        #         'form_3': QuizPhoneForm()
+        #     })
 
-            return render(request, self.template_name, {'form': NamePhoneForm})
+        phone = request.POST['phone']
+        name = None
+        detail = None
+        quiz = None
 
-        return render(request, self.template_name, {'form': NamePhoneForm})
+        if 'name' in request.POST:
+            name = request.POST['name']
+
+        if 'detail' in request.POST:
+            detail = request.POST['detail']
+
+        if 'quiz' in request.POST:
+            quiz = request.POST['quiz']
+
+
+
+        return render(request, self.template_name, {
+            'form_1': NamePhoneForm(),
+            'form_2': DetailPhoneForm(),
+            'form_3': QuizPhoneForm()
+        })
