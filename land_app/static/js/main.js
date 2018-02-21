3459b2e888c7c4e3f28302b19753cb58f615ff91
etterwin <etterwin.yll@gmail.com>
2018-02-21 10:06:30 +0300

    update

--- land_app/static/js/main.js
+++ land_app/static/js/main.js
@@ -1,35 +1,4 @@
-//создать массив всех активных элементов карусели
-let arr = document.querySelectorAll('.carousel__item');
 
-//массив из картинок для вывода в центральном блоке карусели, всегда 9 штук
-let arrImages = ['assets/pictures/png/photo-1.jpg',
-    'assets/pictures/png/photo-2.jpg',
-    'assets/pictures/png/photo-3.JPG',
-    'assets/pictures/png/photo-4.jpg',
-    'assets/pictures/png/photo-1.jpg',
-    'assets/pictures/png/photo-2.jpg',
-    'assets/pictures/png/photo-3.JPG',
-    'assets/pictures/png/photo-4.jpg',
-    'assets/pictures/png/photo-1.jpg'];
-
-//проверка
-console.log(arr.length);
-console.log(arrImages.length);
-
-//замена картинки
-function carouselChange() {
-    for(let i = 0; i < arr.length - 1 ; i++) {
-        for(let j = 0; j < arrImages.length; j++) {
-            if (i === j) {
-                //изменяем атрибут центральной картинки
-                document.getElementById('carousel-img').setAttribute('src', arrImages[j]);
-            }
-            else {
-                return true;
-            }
-        }
-    }
-}
 
 $(document).ready(function () {
     $("#start").click(function () {
